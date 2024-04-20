bc713f9454f036b44a93c83c5e90fc9b024183e7
elshodatc111 <elshodatc1116@gmail.com>
2024-04-20 04:09:14 +0300

    Save

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -6,18 +6,18 @@
     <title>@yield('title')</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
-    <link href="https://atko.tech/NiceAdmin/assets/img/favicon.png" rel="icon">
-    <link href="https://atko.tech/NiceAdmin/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="{{ env('HTTP_URL')}}assets/img/favicon.png" rel="icon">
+    <link href="{{ env('HTTP_URL')}}assets/img/apple-touch-icon.png" rel="apple-touch-icon">
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-    <link href="https://atko.tech/NiceAdmin/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://atko.tech/NiceAdmin/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="https://atko.tech/NiceAdmin/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="https://atko.tech/NiceAdmin/assets/vendor/quill/quill.snow.css" rel="stylesheet">
-    <link href="https://atko.tech/NiceAdmin/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-    <link href="https://atko.tech/NiceAdmin/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="{{ env('HTTP_URL')}}assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ env('HTTP_URL')}}assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="{{ env('HTTP_URL')}}assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="{{ env('HTTP_URL')}}assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="{{ env('HTTP_URL')}}assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="{{ env('HTTP_URL')}}assets/vendor/remixicon/remixicon.css" rel="stylesheet">
     <link href="https://atko.tech/NiceAdmin/assets/vendor/simple-datatables/style.css" rel="stylesheet">
-    <link href="https://atko.tech/NiceAdmin/assets/css/style.css" rel="stylesheet">
+    <link href="{{ env('HTTP_URL')}}assets/css/style.css" rel="stylesheet">
 </head>
 
 <body>
@@ -34,15 +34,15 @@
      </main>
   
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-    <script src="https://atko.tech/NiceAdmin/assets/vendor/apexcharts/apexcharts.min.js"></script>
-    <script src="https://atko.tech/NiceAdmin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="https://atko.tech/NiceAdmin/assets/vendor/chart.js/chart.umd.js"></script>
-    <script src="https://atko.tech/NiceAdmin/assets/vendor/echarts/echarts.min.js"></script>
-    <script src="https://atko.tech/NiceAdmin/assets/vendor/quill/quill.min.js"></script>
-    <script src="https://atko.tech/NiceAdmin/assets/vendor/simple-datatables/simple-datatables.js"></script>
-    <script src="https://atko.tech/NiceAdmin/assets/vendor/tinymce/tinymce.min.js"></script>
-    <script src="https://atko.tech/NiceAdmin/assets/vendor/php-email-form/validate.js"></script>
-    <script src="https://atko.tech/NiceAdmin/assets/js/main.js"></script>
+    <script src="{{ env('HTTP_URL')}}assets/vendor/apexcharts/apexcharts.min.js"></script>
+    <script src="{{ env('HTTP_URL')}}assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ env('HTTP_URL')}}assets/vendor/chart.js/chart.umd.js"></script>
+    <script src="{{ env('HTTP_URL')}}assets/vendor/echarts/echarts.min.js"></script>
+    <script src="{{ env('HTTP_URL')}}assets/vendor/quill/quill.min.js"></script>
+    <script src="{{ env('HTTP_URL')}}assets/vendor/simple-datatables/simple-datatables.js"></script>
+    <script src="{{ env('HTTP_URL')}}assets/vendor/tinymce/tinymce.min.js"></script>
+    <script src="{{ env('HTTP_URL')}}assets/vendor/php-email-form/validate.js"></script>
+    <script src="{{ env('HTTP_URL')}}assets/js/main.js"></script>
 </body>
 
 </html>
