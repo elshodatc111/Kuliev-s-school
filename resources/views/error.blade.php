44e1f78a358b45eddf204d27b2e671c6b90b8db6
USER <elshodatc111>
2024-04-24 11:38:57 +0500

    Saves

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -21,6 +21,7 @@
     <section class="section error-404 min-vh-100 d-flex flex-column align-items-center justify-content-center">
         <h1>404</h1>
         <h2>Tizim bloklangan mamuriyat bilan bog'laning.</h2>
+        <a class="btn" href="{{ route('home') }}">Back to home</a>
     </section>
 </body>
 </html>
